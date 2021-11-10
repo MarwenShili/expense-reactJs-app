72597f98b31ce69ec5f1607999d2956c94731496
Marwen Shili <marweneshili@takiacademyteam.com>
2021-11-10 12:31:58 +0100

    add dynamic chart expense

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,6 +1,10 @@
+import React, { useState } from 'react';
+
+
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 const NewExpense =(props) =>{
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -10,10 +14,25 @@ const NewExpense =(props) =>{
 
   };
 
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
     return(
     <div className='new-expense'>
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />    
-    </div>
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense </button>
+      )}
+ {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}    </div>
     )
 }
 
